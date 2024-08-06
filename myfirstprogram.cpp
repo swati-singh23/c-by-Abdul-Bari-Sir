319b765d67667e72bc102aabebf2a0d2685d40d3
swati singh 23 <singhswati8219860@gmail.com>
2024-08-06 22:41:33 +0530

    my first program

---
+++ myfirstprogram.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+cout<<"enter two numbers";
+
+cin>>a>>b;
+
+c=a+b;
+
+cout<<"sum is"<<c<<endl;
+
+return 0;
+
+}
+
