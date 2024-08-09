7424cdb61007e0621216613977cf5d9aed7f9d30
swati singh 23 <singhswati8219860@gmail.com>
2024-08-10 00:59:36 +0530

    maximum and minimum among two numbers

---
+++ if_else_max_of two_num.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int x,y;
+
+   cout<<"enter two numbers" <<endl;
+   cin>>x>>y;
+   if(x>y)
+      cout<<"Max is"<<x;
+   else
+    cout<<"Min is "<<x;
+
+    return 0;
+}
