dfa38e7a9653704e985937c7fa47ff5c72e6a7b5
swati singh 23 <singhswati8219860@gmail.com>
2024-08-09 08:59:52 +0530

    enum and typedef

---
+++ Enum and typedef.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+enum day {mon=10 ,tue=7,wed=9,thu,fri,sat,sun};
+enum dept {cs,it,ec,mech};
+
+
+int main()
+{
+  day d;
+  d=tue;
+
+
+  dept dep = cs;
+  cout<<mon<<endl;
+  cout<<tue<<endl;
+  cout<<wed<<endl;
+  cout<<thu<<endl;
+  cout<<fri<<endl;
+  cout<<sat<<endl;
+  cout<<sun<<endl;
+
+
+
+    return 0;
+}
---
+++ enum2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef int marks;
+
+int main()
+{  
+    
+    int m1,m2;
+
+    m1=50;
+    m2=90;
+
+    cout<<m1;
+
+    return 0;
+}
+
