ec76154feec1d8af88166c34e86792d18aa738cd
swati singh 23 <singhswati8219860@gmail.com>
2024-08-12 07:42:12 +0530

    working and leisure if else

---
+++ compound_ifelse_7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int hour;
+    cout<<"enter hours";
+    cin>>hour;
+    if(hour>=9 && hour <=18)
+       cout<<"working";
+    else 
+       cout<<"leisure";
+         
+    return 0;
+}
