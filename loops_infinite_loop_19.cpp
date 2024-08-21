75176b5f06f4cd7f539adb2d953f7a2c90f65723
swati singh 23 <singhswati8219860@gmail.com>
2024-08-21 16:37:26 +0530

    infinite loop

---
+++ loops_infinite_loop_19.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+ 
+   int i=0;
+    
+   for(;;)
+   {
+     cout<<"Hello/n";
+     i++;
+
+   }
+
+
+
+
+
+
+
+
+    return 0;
+}
