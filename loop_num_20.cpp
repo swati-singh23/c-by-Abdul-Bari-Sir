2a6772783373dd1e6823587b04747dbc47675118
swati singh 23 <singhswati8219860@gmail.com>
2024-08-21 17:06:09 +0530

    printing numbers 1 to 10 using while loop

---
+++ loop_num_20.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    //printing numbers 1 to 10 using while loop
+    int n,i=0;
+    cout<<"enter n";
+    cin>>n; 10
+    while(i<=n)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+
+    return 0;
+}
