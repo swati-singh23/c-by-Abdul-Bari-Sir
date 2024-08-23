8abdf376efb00b7d75644bd14aa2273b717df230
swati singh 23 <singhswati8219860@gmail.com>
2024-08-23 23:35:59 +0530

    multiplication table

---
+++ multiplication_22.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+	int n,i;
+	cout<<"Enter a Number";
+	cin>>n;
+	    
+	for(i=1;i<=10;i++)
+	{
+	    cout<<n<<" x "<<i<<" = "<<i*n<<endl;
+	}
+	    
+	return 0;
+	
+}
