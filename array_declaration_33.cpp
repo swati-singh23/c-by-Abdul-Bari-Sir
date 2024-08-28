bb92fdf4a8686ac99963596ea5856eecada63541
swati singh 23 <singhswati8219860@gmail.com>
2024-08-28 22:13:42 +0530

    array declaration

---
+++ array_declaration_33.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[]={2,5,9,8,7};
+
+    for (auto  x:a)
+        cout<<x<<endl;
+
+        return 0;
+        
+}
