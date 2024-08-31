c98c5e40b21c8c5791a4abc6d49725630fe6eac7
swati singh 23 <singhswati8219860@gmail.com>
2024-08-31 20:10:09 +0530

    pattern1

---
+++ pattern1_41.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int i,j,count=0;
+
+    for(i=0;i<=4;i++)
+    {
+        for(j=0;j<=4;j++)
+        {
+           cout<<count<<" ";
+           count++;
+        }
+        cout<<endl;
+    }
+   
+
+    return 0;
+}
