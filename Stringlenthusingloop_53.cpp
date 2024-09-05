b3905ce65b3da507b2e5d34e04a377b3be230042
swati singh 23 <singhswati8219860@gmail.com>
2024-09-05 23:29:50 +0530

    STRING Length using loop

---
+++ Stringlenthusingloop_53.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string str="welcome";
+	int count=0;
+	    
+	for(int i=0;str[i]!='\0';i++)
+	{
+	    count++;
+	}
+	cout<<"Length is"<<count<<endl;
+	    
+	return 0;
+	    
+}
