8edaef29da487934c1440ef48daa28935d975e12
swati singh 23 <singhswati8219860@gmail.com>
2024-09-07 01:23:33 +0530

    Call by address

---
+++ callbyaddress_64.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+int temp;
+temp=*x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+int a=10, b=20;
+swap(&a,&b);
+cout<<a<<" "<<b;
+
+return 0;
+}
