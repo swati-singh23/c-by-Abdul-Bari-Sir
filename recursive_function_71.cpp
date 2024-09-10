e9fff5ac762dc497639ebdd531c9c8850b8db3df
swati singh 23 <singhswati8219860@gmail.com>
2024-09-10 19:04:19 +0530

    recursive function

---
+++ recursive_function_71.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun(int n)
+{
+  if(n>0)
+  {
+    cout<<n<<endl;
+    fun(n-1);
+  }
+}
+int main()
+{
+  int x=5;
+  fun(x);
+
+  return 0;
+}
